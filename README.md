66e93b0f871cc22550a64169d1b187ee1cdd0136
Taylor Buckner <taylora.buckner@gmail.com>
2017-10-27 16:22:10 -0500

    added a readme and got a basic gulp process running

---
+++ README.md
@@ -0,0 +1,53 @@
+# Support Email Gulp Process
+
+Simple package utilizing Gulp tasks to more easily maintain our email templates. Features Browser Sync, minification, and exports two files. 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+Node/npm
+https://nodejs.org/en/download/
+
+```
+computer.do /.//.. install a thing
+```
+
+### Installing
+
+Setup for this package is designed to be simple and transferrable.
+
+Install package
+
+```
+npm install
+```
+
+## Usage
+
+Browser will automatically launch localhost server and refresh on save to aid in development speed. When happy with results build the project and use dist files.
+
+### Deploy
+
+Initial command to get the local server running and auto updating
+
+```
+gulp
+```
+
+### Build
+
+Once you're happy with your results, CTRL(or CMD)+C to cancel server task
+
+```
+^C
+```
+```
+gulp build
+```
+
+## Authors
+
+* **Taylor Buckner** - *whatever...* - [tabuckner](https://github.com/tabuckner)
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+
+
+gulp.task('concat', function () {
+  return gulp.src(['app/header.html', 'app/content.html', 'app/footer.html'])
+    .pipe(concat('index.html'))
+    .pipe(gulp.dest('./tmp/'));
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./tmp"
+    },
+    port: 6969 //lol
+  });
+});
+
+gulp.task('default', ['serve'], function () {
+  gulp.watch('./app/*', ['concat']);
+  gulp.watch('tmp/*').on('change', reload);
+});
+
+gulp.task('htmlMin', function () {
+  return gulp.src('tmp/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist/html'));
+});
+
+gulp.task('imgMin', () =>
+  gulp.src('app/images/*')
+    .pipe(imagemin([
+      imagemin.gifsicle({
+        interlaced: true,
+        colors: 48,
+        optimizationLevel: 3
+      })
+    ]))
+    .pipe(gulp.dest('dist/images'))
+);
+
+gulp.task('build', ['imgMin', 'htmlMin']);
